f2f3336b585c3c996ff39f072e679e17158f81c1
LUJIAN <504439788@qq.com>
2024-08-22 10:05:12 +0800

    添加异常类型

---
+++ UsbSerialForAndroid.Net/Exceptions/BulkTransferException.cs
@@ -0,0 +1,38 @@
+﻿using Android.Hardware.Usb;
+using System;
+
+namespace UsbSerialForAndroid.Net.Exceptions
+{
+    public class BulkTransferException : Exception
+    {
+        public UsbEndpoint? Endpoint { get; }
+        public byte[]? Buffer { get; }
+        public int Offset { get; }
+        public int Length { get; }
+        public int Timeout { get; }
+        public int Result { get; }
+        public BulkTransferException(string message, int result, UsbEndpoint? endpoint, byte[]? buffer, int offset, int length, int timeout)
+            : base(GetMessage(message, result, endpoint, buffer, offset, length, timeout))
+        {
+            Result = result;
+            Endpoint = endpoint;
+            Buffer = buffer;
+            Offset = offset;
+            Length = length;
+            Timeout = timeout;
+        }
+        public BulkTransferException(string message, int result, UsbEndpoint? endpoint, byte[]? buffer, int length, int timeout)
+            : base(GetMessage(message, result, endpoint, buffer, length, timeout))
+        {
+            Result = result;
+            Endpoint = endpoint;
+            Buffer = buffer;
+            Length = length;
+            Timeout = timeout;
+        }
+        private static string GetMessage(string message, int result, UsbEndpoint? endpoint, byte[]? buffer, int offset, int length, int timeout)
+            => $"{message},Result:{result},Address:{endpoint?.Address},Direction:{endpoint?.Direction},EndpointNumber:{endpoint?.EndpointNumber},Type:{endpoint?.Type},MaxPacketSize:{endpoint?.MaxPacketSize},Buffer:{BitConverter.ToString(buffer ?? Array.Empty<byte>())},Offset:{offset},Length:{length},Timeout:{timeout}";
+        private static string GetMessage(string message, int result, UsbEndpoint? endpoint, byte[]? buffer, int length, int timeout)
+             => $"{message},Result:{result},Address:{endpoint?.Address},Direction:{endpoint?.Direction},EndpointNumber:{endpoint?.EndpointNumber},Type:{endpoint?.Type},MaxPacketSize:{endpoint?.MaxPacketSize},Buffer:{BitConverter.ToString(buffer ?? Array.Empty<byte>())},Length:{length},Timeout:{timeout}";
+    }
+}
---
+++ UsbSerialForAndroid.Net/Exceptions/ControlTransferException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UsbSerialForAndroid.Net.Exceptions
+{
+    public class ControlTransferException : Exception
+    {
+        public int RequestType { get; }
+        public int Request { get; }
+        public int Value { get; }
+        public int Index { get; }
+        public byte[]? Buffer { get; }
+        public int Offset { get; }
+        public int Length { get; }
+        public int Timeout { get; }
+        public int Result { get; }
+        public ControlTransferException(string message, int result, int requestType, int request, int value, int index, byte[]? buffer, int length, int timeout)
+            : base(GetMessage(message, result, requestType, request, value, index, buffer, length, timeout))
+        {
+            Result = result;
+            RequestType = requestType;
+            Request = request;
+            Value = value;
+            Index = index;
+            Buffer = buffer;
+            Length = length;
+            Timeout = timeout;
+        }
+        public ControlTransferException(string message, int result, int requestType, int request, int value, int index, byte[]? buffer, int offset, int length, int timeout)
+            : base(GetMessage(message, result, requestType, request, value, index, buffer, offset, length, timeout))
+        {
+            Result = result;
+            RequestType = requestType;
+            Request = request;
+            Value = value;
+            Index = index;
+            Buffer = buffer;
+            Offset = offset;
+            Length = length;
+            Timeout = timeout;
+        }
+        private static string GetMessage(string message, int result, int requestType, int request, int value, int index, byte[]? buffer, int offset, int length, int timeout)
+            => $"{message},Result:{result},RequestType:{requestType},Request:{request},Value:{value},Index:{index},Buffer:{BitConverter.ToString(buffer ?? Array.Empty<byte>())},Offset:{offset},Length:{length},Timeout:{timeout}";
+        private static string GetMessage(string message, int result, int requestType, int request, int value, int index, byte[]? buffer, int length, int timeout)
+            => $"{message},Result:{result},RequestType:{requestType},Request:{request},Value:{value},Index:{index},Buffer:{BitConverter.ToString(buffer ?? Array.Empty<byte>())},Length:{length},Timeout:{timeout}";
+    }
+}
